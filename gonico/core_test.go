8a0196becaee698f5ece8f327effc7865cb8302d
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-10-26 17:55:51 +0900

    Add test case

--- gonico/core_test.go
+++ gonico/core_test.go
@@ -5,16 +5,23 @@ import (
 	"testing"
 )
 
-const (
-	videoId = "sm9"
-)
-
 func TestGetVideoThumbResponse(t *testing.T) {
-	result, status := GetVideoThumbResponse(videoId)
+	result, status := GetVideoThumbResponse("sm1")
+	switch status {
+	case "fail":
+		errorInfo := result.ErrorInfo
+		assert.Equal(t, errorInfo.Code, "DELETED")
+		assert.Equal(t, errorInfo.Description, "deleted")
+	default:
+		t.Fail()
+	}
+
+	result, status = GetVideoThumbResponse("sm9")
 	switch status {
 	case "ok":
 		videoInfo := result.VideoInfo
-		assert.Equal(t, videoInfo.VideoId, videoId)
+		assert.Equal(t, videoInfo.VideoId, "sm9")
+		assert.Equal(t, videoInfo.MovieType, "flv")
 	default:
 		t.Fail()
 	}
