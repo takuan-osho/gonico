71a4b34504c0ad8fe478c599c772e8581e47a1be
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-10-27 17:27:27 +0900

    Replace all unit tests with BDD style tests in user_test.go

--- user_test.go
+++ user_test.go
@@ -1,16 +1,40 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"net/http"
 )
 
-func TestLogin(t *testing.T) {
-	resp, err := Login()
-	authFlag := resp.Header["X-Niconico-Authflag"][0]
-	if err != nil {
-		assert.Equal(t, authFlag, 0)
-	} else {
-		assert.NotEqual(t, authFlag, 0)
-	}
-}
+var _ = Describe("gonico user test", func() {
+
+	Describe("test Login function", func() {
+		var (
+			resp     *http.Response
+			err      error
+			authFlag string
+		)
+
+		BeforeEach(func() {
+			resp, err = Login()
+			authFlag = resp.Header["X-Niconico-Authflag"][0]
+		})
+
+		Context("when login error occurred", func() {
+			if err != nil {
+				It("should return 0", func() {
+					Expect(authFlag).To(Equal("0"))
+				})
+			}
+		})
+
+		Context("when login success occurred", func() {
+			if err == nil {
+				It("should not return 0", func() {
+					Expect(authFlag).NotTo(Equal("0"))
+				})
+			}
+		})
+
+	})
+})
